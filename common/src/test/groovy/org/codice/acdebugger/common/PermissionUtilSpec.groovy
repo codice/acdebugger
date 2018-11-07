918f4737600b1a5fe734bfa14d129a12ff0ecf8f
Patrick Ouellet <patrick.ouellet@connexta.com>
2018-11-07 07:40:24 -0700

    ACD-22 Fixed test case on windows

--- common/src/test/groovy/org/codice/acdebugger/common/PermissionUtilSpec.groovy
+++ common/src/test/groovy/org/codice/acdebugger/common/PermissionUtilSpec.groovy
@@ -39,19 +39,19 @@ class PermissionUtilSpec extends Specification {
       permission.actions == (((actions == null) || actions.empty) ? null : actions)
 
     where:
-      with_what                                                 || clazz              | name                 | actions              || result
-      "a file permission class"                                 || FilePermission     | "path"               | "read,write"         || "java.io.FilePermission \"path\", \"read,write\""
-      "a property permission class"                             || PropertyPermission | "property"           | "read"               || "java.util.PropertyPermission \"property\", \"read\""
-      "null as actions"                                         || FilePermission     | "C:\\path"           | null                 || "java.io.FilePermission \"C:\\\\path\""
-      "empty actions"                                           || FilePermission     | "path/child"         | ""                   || "java.io.FilePermission \"path/child\""
-      "character 0x7 in the name and actions"                   || FilePermission     | "path\u0007"         | "read\u0007"         || "java.io.FilePermission \"path\\a\", \"read\\a\""
-      "character \\b in the name and actions"                   || FilePermission     | "path\b"             | "read\b"             || "java.io.FilePermission \"path\\b\", \"read\\b\""
-      "character \\f in the name and actions"                   || FilePermission     | "path\f"             | "read\f"             || "java.io.FilePermission \"path\\f\", \"read\\f\""
-      "character \\n in the name and actions"                   || FilePermission     | "path\n"             | "read\n"             || "java.io.FilePermission \"path\\n\", \"read\\n\""
-      "character \\r in the name and actions"                   || FilePermission     | "path\r"             | "read\r"             || "java.io.FilePermission \"path\\r\", \"read\\r\""
-      "character \\t in the name and actions"                   || FilePermission     | "path\t"             | "read\t"             || "java.io.FilePermission \"path\\t\", \"read\\t\""
-      "character 0xB in the name and actions"                   || FilePermission     | "path\u000B"         | "read\u000b"         || "java.io.FilePermission \"path\\v\", \"read\\v\""
-      "character \\ in the name and actions"                    || FilePermission     | "path\\"             | "read\\"             || "java.io.FilePermission \"path\\\\\", \"read\\\\\""
-      "character outside and on the limit of the regular range" || FilePermission     | "path é\u0002\u007f" | "read é\u0002\u007F" || "java.io.FilePermission \"path \\351\\2\\177\", \"read \\351\\2\\177\""
+      with_what                                                 || clazz              | name                      | actions                   || result
+      "a file permission class"                                 || FilePermission     | "path"                    | "read,write"              || "java.io.FilePermission \"path\", \"read,write\""
+      "a property permission class"                             || PropertyPermission | "property"                | "read"                    || "java.util.PropertyPermission \"property\", \"read\""
+      "null as actions"                                         || FilePermission     | "C:\\path"                | null                      || "java.io.FilePermission \"C:\\\\path\""
+      "empty actions"                                           || FilePermission     | "path/child"              | ""                        || "java.io.FilePermission \"path/child\""
+      "character 0x7 in the name and actions"                   || FilePermission     | "path\u0007"              | "read\u0007"              || "java.io.FilePermission \"path\\a\", \"read\\a\""
+      "character \\b in the name and actions"                   || FilePermission     | "path\b"                  | "read\b"                  || "java.io.FilePermission \"path\\b\", \"read\\b\""
+      "character \\f in the name and actions"                   || FilePermission     | "path\f"                  | "read\f"                  || "java.io.FilePermission \"path\\f\", \"read\\f\""
+      "character \\n in the name and actions"                   || FilePermission     | "path\n"                  | "read\n"                  || "java.io.FilePermission \"path\\n\", \"read\\n\""
+      "character \\r in the name and actions"                   || FilePermission     | "path\r"                  | "read\r"                  || "java.io.FilePermission \"path\\r\", \"read\\r\""
+      "character \\t in the name and actions"                   || FilePermission     | "path\t"                  | "read\t"                  || "java.io.FilePermission \"path\\t\", \"read\\t\""
+      "character 0xB in the name and actions"                   || FilePermission     | "path\u000B"              | "read\u000b"              || "java.io.FilePermission \"path\\v\", \"read\\v\""
+      "character \\ in the name and actions"                    || FilePermission     | "path\\"                  | "read\\"                  || "java.io.FilePermission \"path\\\\\", \"read\\\\\""
+      "character outside and on the limit of the regular range" || FilePermission     | "path \u0082\u0002\u007f" | "read \u0082\u0002\u007F" || "java.io.FilePermission \"path \\202\\2\\177\", \"read \\202\\2\\177\""
   }
 }
